c6b2bbfc3ff742efc5f0199d08ab6b9e4c903894
NIGHTINGALE1411 <92319401+NIGHTINGALE1411@users.noreply.github.com>
2022-06-28 20:59:07 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,10 +1,27 @@
 
 //ADD YOUR FIREBASE LINKS HERE
 
-function getData() {firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
-       Room_names = childKey;
-      //Start code
 
+      //Start code
+      var firebaseConfig = {
+            apiKey: "AIzaSyDNxoME0pEVBxS_KOT7-1dVhhgr1h3DBkI",
+            authDomain: "letschatwebapp-dbc4d.firebaseapp.com",
+            databaseURL: "https://letschatwebapp-dbc4d-default-rtdb.firebaseio.com",
+            projectId: "letschatwebapp-dbc4d",
+            storageBucket: "letschatwebapp-dbc4d.appspot.com",
+            messagingSenderId: "173959192046",
+            appId: "1:173959192046:web:3cdf57e255fccbb5a442ff",
+            measurementId: "G-1GCV2GZTEY"
+          };
+          
+          // Initialize Firebase
+         firebase.initializeApp(firebaseConfig);
+          
       //End code
-      });});}
-getData();
+      function Addroom(){
+            roomname=document.getElementById("roomname").value;
+            firebase.database().ref("/").child(roomname).update({
+                purpose:"addingroom"
+            });
+      };;
+
